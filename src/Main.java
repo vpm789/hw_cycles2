601239a9151f6437aa84bb9996216799686c4927
vpm789 <vpm789@gmail.com>
2023-02-08 20:25:49 +0300

    task4-8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1() {
@@ -14,7 +22,9 @@ public class Main {
             total = total + total / 100;
             total = total + deposit;
             month = month + 1;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            if (month % 12 == 0 || total >= 2459000) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            }
         }
 
     }
@@ -31,6 +41,7 @@ public class Main {
         for (i = 10; i >= 1; i = i - 1) {
             System.out.print(i + " ");
         }
+        System.out.println("");
     }
 
     public static void task3() {
@@ -48,4 +59,84 @@ public class Main {
         while (i < 10);
     }
 
+    public static void task4() {
+        System.out.println("");
+        System.out.println("Задача №4");
+        int deposit = 15000;
+        int percent = 7;
+        int total = 0;
+        int month = 0;
+        while (total <= 12000000) {
+            total = (int) (total + (total * 0.07));
+            total = total + deposit;
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("");
+        System.out.println("Задача №5");
+        int deposit = 15000;
+        int percent = 7;
+        int total = 0;
+        int month = 0;
+        while (total <= 12000000) {
+            total = (int) (total + (total * 0.07));
+            total = total + deposit;
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            }
+        }
+    }
+
+    public static void task6() {
+        System.out.println("");
+        System.out.println("Задача №6");
+        int deposit = 15000;
+        int percent = 7;
+        int total = 0;
+        int month = 0;
+        while (month <= 12 * 9) {
+            total = (int) (total + (total * 0.07));
+            total = total + deposit;
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            }
+        }
+
+    }
+
+    public static void task7() {
+        System.out.println("");
+        System.out.println("Задача №7");
+        int[] PossibleDayFirstFriday = {1, 2, 3, 4, 5, 6, 7};
+        int dayOfFirstFriday = new Random().nextInt(PossibleDayFirstFriday.length + 1);
+        int daysInMonth = 31;
+        do {
+            System.out.println("Сегодня пятница, " + dayOfFirstFriday + "-е число. Необходимо подготовить отчет");
+            dayOfFirstFriday = dayOfFirstFriday + 7;
+        }
+        while (dayOfFirstFriday < daysInMonth);
+    }
+
+    public static void task8() {
+        System.out.println("");
+        System.out.println("Задача №8");
+        int cometYear = 0;
+        int cometPeriod = 79;
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+        int lowYear = currentYear - 200;
+        int highYear = currentYear + 100;
+        while (cometYear < currentYear) {
+            cometYear = cometYear + cometPeriod;
+            if (cometYear > lowYear && cometYear < highYear) {
+                System.out.println(cometYear);
+            }
+        }
+
+    }
 }
